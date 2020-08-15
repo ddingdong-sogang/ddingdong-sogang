1367c088c25d38af169b6a8e0c95020082528fba
asomeJay <45970468+asomeJay@users.noreply.github.com>
2020-08-16 04:07:26 +0900

    :tada: initial commit

---
+++ ddsogang/src/main/java/ddingddong/ddsogang/DdsogangApplication.java
@@ -0,0 +1,13 @@
+package ddingddong.ddsogang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdsogangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdsogangApplication.class, args);
+	}
+
+}
---
+++ ddsogang/src/test/java/ddingddong/ddsogang/DdsogangApplicationTests.java
@@ -0,0 +1,13 @@
+package ddingddong.ddsogang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdsogangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
